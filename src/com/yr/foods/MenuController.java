e6736d245b57982f2b207ec1ff145905391ede84
kyr1936 <48780800+kyr1936@users.noreply.github.com>
2019-03-27 14:53:37 +0900

    Initial commit

--- src/com/yr/foods/MenuController.java
+++ src/com/yr/foods/MenuController.java
@@ -7,36 +7,38 @@ public class MenuController {		// 메뉴 선택
 	public void start() {
 		// 1. 메뉴등록
 		MenuAdd menuAdd = new MenuAdd();
-		
+
 		boolean check = true;
 		Scanner sc = new Scanner(System.in);
 		MenuView menuCount = new MenuView();
-		
+
 		Food[] foods = menuAdd.add();
-		
-		
+		int [] counts = new int[foods.length];
+
 		while(check) {
 			System.out.println("1. 주문   2. 결산");
 			int select = sc.nextInt();
-			
+
 			if (select==1) {
 				for(int i=0; i<foods.length; i++) {
 					System.out.println(i+1 +".   "+ foods[i].name + ",  " + foods[i].price);
-					
+
 				} System.out.println(foods.length+1 + ". 주문하기");
-					select =sc.nextInt();
-				
-				
+				select =sc.nextInt();
+				System.out.println("수량 입력");
+				int c = sc.nextInt();
+
+
 			} else {
 				menuCount.count();
-				
+
 				check =!check;
 			}	
-			
-			
-			
-			
-			
+
+
+
+
+
 		}
 	}
 }
